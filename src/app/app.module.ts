68aaabfbbd8f4dcd97f44acdcf0816dd8cb2b0a6
Pratibha <misspratibhaa@gmail.com>
2022-12-23 13:57:24 +0530

    creating e-shop website under Code Better Guidance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LandingPageHeaderComponent } from './landing-page-header/landing-page-header.component';
+import { LandingPageFooterComponent } from './landing-page-footer/landing-page-footer.component';
+import { AdminAuthComponent } from './admin-auth/admin-auth.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LandingPageHeaderComponent,
+    LandingPageFooterComponent,
+    AdminAuthComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
